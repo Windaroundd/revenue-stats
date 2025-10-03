f92ea12b6de6ab9e2ed0ddd4c3b81d993c625a2e
Phong huynh <phonghqm@gmail.com>
2025-10-03 19:49:00 +0700

    feat: remove unneed user root

--- apps/api/src/routes/userRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { createUser, listUsers } from "../controllers/userController";
-
-const router = Router();
-
-router.get("/", listUsers);
-router.post("/", createUser);
-
-export default router;
